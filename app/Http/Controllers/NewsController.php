e3f9ba8293a52eb10bf5d04aaaec3d635b32e647
mikairyuu <miva200267@gmail.com>
2021-10-15 23:08:44 +1000

    Lesson 4: Add news routes, controller and views (#3)

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+
+class NewsController extends Controller
+{
+    public function getList()
+    {
+        $news = News::query()
+            ->whereDate('published_at', '<=', date('Y-m-d H:i:s'))
+            ->where('is_published', true)
+            ->orderByDesc('published_at')
+            ->orderByDesc('id')
+            ->paginate(5);
+
+        return view('newsList', ['news' => $news]);
+    }
+
+    public function getDetails(string $slug)
+    {
+        $newsEntry = News::query()
+            ->where('slug', $slug)
+            ->whereDate('published_at', '<=', date('Y-m-d H:i:s'))
+            ->where('is_published', true)
+            ->first();
+
+        if ($newsEntry === null)
+            abort(404);
+        return view('news', ['newsEntry' => $newsEntry]);
+    }
+}
