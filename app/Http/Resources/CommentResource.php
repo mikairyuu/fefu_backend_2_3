d62b9adb8967de33e0f105d66eb7aaef07174f26
mikairyuu <miva200267@gmail.com>
2021-11-17 21:57:12 +1000

    Lesson 9: Add posts and comments with API

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'text' => $this->text,
+            'created_at' => $this->created_at->format('d-m-Y H:i'),
+            'updated_at' => $this->updated_at->format('d-m-Y H:i'),
+            'author' => new UserResource($this->user),
+        ];
+    }
+}
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function run()
+    {
+        Post::query()->truncate();
+        Post::factory(random_int(10, 20))->create();
+    }
+}
